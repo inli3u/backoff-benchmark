1311ffce81ba4a0f6807e490cbac86017d0a1af5
Alex Dunn <alex@logicallydunn.com>
2023-09-06 22:43:20 -0400

    Implemented backoff options

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,53 @@
-import { exponentialBackoff } from './';
+import { exponentialBackoff, retryWait } from './';
 
 describe('exponentialBackoff()', () => {
-  test('initial test', () => {
-    const backoff = exponentialBackoff({});
-    expect(backoff(-5)).toBe(1);
-    expect(backoff(0)).toBe(1);
-    expect(backoff(5)).toBe(32);
+  test('retries', () => {
+    const backoff = exponentialBackoff({
+      start: 1_000,
+      jitterPercent: 0,
+    });
+    expect(backoff(-5)).toBe(1_000);
+    expect(backoff(0)).toBe(1_000);
+    expect(backoff(5)).toBe(32_000);
+  });
+
+  test('base', () => {
+    const backoff = exponentialBackoff({
+      base: 1.5,
+      start: 1_000,
+      jitterPercent: 0,
+    });
+    expect(backoff(0)).toBe(1_000);
+    expect(backoff(1)).toBe(1_500);
+    expect(Math.floor(backoff(5))).toBe(7_593);
+  });
+
+  test('ceiling', () => {
+    const ceiling = 10_000;
+    const backoff = exponentialBackoff({
+      start: 1_000,
+      jitterPercent: 0,
+      ceiling,
+    });
+    expect(backoff(0)).toBe(1_000);
+    expect(backoff(1)).toBe(2_000);
+    expect(backoff(5)).toBe(ceiling);
+  });
+});
+
+describe('retryWait()', () => {
+  test('Promise resolves after specified time', async () => {
+    // delay in miliseconds
+    const expectedDelay = 100;
+
+    // We'll give retryWait an identity function so that we can tell it exactly how long to wait.
+    const wait = retryWait((n) => n);
+
+    const start = Date.now();
+    await wait(expectedDelay);
+    const actualDelay = Date.now() - start;
+
+    // Expect actual delay to be within 20 ms of expected delay
+    expect(Math.abs(actualDelay - expectedDelay)).toBeLessThan(20);
   })
 });
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,58 @@
 type RetryFn = (retries: number) => number;
-type RetryWaitFn = (retries: number) => Promise<void>;
+type WaitFn = (retries: number) => Promise<void>;
 
 interface ExponentialBackoffOpts {
   base?: number;
+  start?: number;
   ceiling?: number;
   jitterPercent?: number;
   jitterBias?: number;
-  jitterTime?: 'early' | 'late'
+  jitterRandomize?: 'once' | 'each';
 }
 
-export const exponentialBackoff = (opts: ExponentialBackoffOpts): RetryFn => {
-  return (retries: number) => {
-    retries = Math.max(0, retries);
-    return 2 ** retries;
+type ExponentialBackoffFn = (opts: ExponentialBackoffOpts) => RetryFn;
+
+/**
+ * Builds and returns a backoff function.
+ */
+export const exponentialBackoff: ExponentialBackoffFn = ({
+  base = 2,
+  start = 1_000,
+  ceiling = Number.POSITIVE_INFINITY,
+  jitterPercent = 1,
+  jitterBias = 0.5,
+  jitterRandomize = 'once',
+}) => {
+  let random = Math.random;
+
+  if (jitterRandomize === 'once') {
+    const n = Math.random();
+    random = () => n;
+  }
+
+  return (retries) => {
+    retries = Math.max(retries, 0);
+
+    let wait = base ** retries * start;
+    wait = Math.min(ceiling, wait);
+
+    if (jitterPercent) {
+      const amount = (wait - start) * jitterPercent;
+      wait += amount * random();
+    }
+
+    return wait;
   };
 };
 
+
+type RetryWaitFn = (fn: RetryFn) => WaitFn;
+
 /**
- * Maps a RetryFn to a RetryWaitFn that returns a promise and can be awaited.
+ * Maps a RetryFn to a WaitFn so that you can await the retry time.
  */
-export const retryWait = (fn: RetryFn): RetryWaitFn => {
-  return (retries: number) => {
+export const retryWait: RetryWaitFn = (fn) => {
+  return (retries) => {
     return new Promise((resolve, reject) => setTimeout(resolve, fn(retries)));
   };
 };
